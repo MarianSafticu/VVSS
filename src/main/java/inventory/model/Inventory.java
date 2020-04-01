cc13faf9ce4e90157e58d5000b075477b7e2f2d1
Andreea Rusu <rusu18andreea@gmail.com>
2020-04-01 13:02:35 +0300

    Change search function

--- src/main/java/inventory/model/Inventory.java
+++ src/main/java/inventory/model/Inventory.java
@@ -100,7 +100,10 @@ public class Inventory {
      */
     public Part lookupPart(String searchItem) {
         for(Part p:allParts) {
-            if(p.getName().contains(searchItem) || (p.getPartId()+"").equals(searchItem)) return p;
+            if(p.getName().contains(searchItem))
+                return p;
+            if((p.getPartId()+"").equals(searchItem))
+                return p;
         }
         return null;
     }
